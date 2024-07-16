b62abf42f46bea6f47f4021db988661b89cfa696
fatwanahar <fatwanahar@gmail.com>
2024-07-16 20:29:41 +0700

    change dummy.js and add dummy.html

--- dummy.js
+++ dummy.js
@@ -29,19 +29,19 @@
 
 //Isogram dimana dalam string tidak ada huruf yang berulang
 
-let nama = "fF";
+// let nama = "fF";
 
-let isogram = (str) => {
-  let sortStr = str.toLowerCase().split("").sort();
-  // let noDups = new Set(sortStr);
-  // return sortStr.length === noDups.size;
-  for (let i = 1; i < sortStr.length; i++) {
-    if (sortStr[i] === sortStr[i - 1]) {
-      return false;
-    }
-  }
-  return true;
-};
+// let isogram = (str) => {
+//   let sortStr = str.toLowerCase().split("").sort();
+//   // let noDups = new Set(sortStr);
+//   // return sortStr.length === noDups.size;
+//   for (let i = 1; i < sortStr.length; i++) {
+//     if (sortStr[i] === sortStr[i - 1]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-// isogram(nama);
-console.log(isogram(nama));
+// // isogram(nama);
+// console.log(isogram(nama));
